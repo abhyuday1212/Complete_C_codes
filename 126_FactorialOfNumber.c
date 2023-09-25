9484054552882cf76e59448dafe566f10d9439d8
Abhyuday Pratap Singh <apsworks1212@gmail.com>
2023-09-25 23:43:58 +0530

    names updated

---
+++ 126_FactorialOfNumber.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+      int a;
+      printf(" enter the number");
+      scanf("%d",&a)
+      printf("the factorial %d is %d",a ,fat(a));
+      return 0;
+      
+
+      return 0;
+}
